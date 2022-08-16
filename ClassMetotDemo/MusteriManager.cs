11a9f6f07c86401e2b9dac74994c791496560a35
Reşit Çiğdem <resitcgdm@gmail.com>
2022-08-16 04:38:25 +0300

    musteritakip

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " :" + musteri.Id + " "+musteri.Yas);
+            Console.WriteLine("Ekleme işlemi tamamlandı");
+            Console.WriteLine("------------");
+        }
+        public void Listele(Musteri musteri) 
+        {
+                Console.WriteLine("Müşteriler listeleniyor"+" "+musteri.Ad+ musteri.Soyad + " :" + musteri.Id + " " + musteri.Yas);
+            Console.WriteLine("Müşteriler Listelendi");
+            Console.WriteLine("-----------------" );
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad+" isimli müşteri siliniyor...");
+            Console.WriteLine("silme işlemi tamamlandı.");
+            Console.WriteLine("-------");
+
+        }
+
+
+
+    }
+
+
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ClassMetotDemo;
+
+
+Musteri musteri1 = new Musteri();
+musteri1.Id = 1;
+musteri1.Ad = "Resit";
+musteri1.Soyad = "Cigdem";
+musteri1.Yas = 26;
+
+Musteri musteri2=new Musteri();
+musteri2.Id = 2;
+musteri2.Ad = "Emin";
+musteri2.Soyad = "Bulut";
+musteri2.Yas = 19;
+
+Musteri musteri3 = new Musteri();
+musteri3.Id = 3;
+musteri3.Ad = "Deniz";
+musteri3.Soyad = "Yaz";
+musteri3.Yas = 28;
+
+Musteri[] musteriler = new Musteri[] { musteri1,musteri2,musteri3 };
+
+foreach (Musteri musteri in musteriler)
+{
+    Console.WriteLine(musteri.Ad +" "+ musteri.Soyad +"  Id:"+ musteri.Id +" Yas:"+ musteri.Yas);
+}
+
+
+
+
+MusteriManager musteriManager= new MusteriManager();
+musteriManager.Ekle( musteri1);
+Console.WriteLine("--------------");
+musteriManager.Listele(musteri1);
+Console.WriteLine("--------------");
+musteriManager.Sil(musteri1);
+Console.WriteLine("--------------");
